d8caa5193507d040f3063e108dcd795ad9202758
Dr. Jochen Manns <dev-2@psimarron.net>
2025-07-06 18:01:05 +0200

    Enforce new version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.jochen_manns.buyitv0"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.11"
+        versionCode = 21
+        versionName = "2.12"
     }
 
     buildTypes {
